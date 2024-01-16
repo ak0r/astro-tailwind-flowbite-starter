005a61d5bc6e10ab38d1653815229887c704a77e
Amit K <androidak0r@gmail.com>
2024-01-16 23:27:05 +0530

    feat: Added Social Icons configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 const owner = "astrojs";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), icon()]
 });
