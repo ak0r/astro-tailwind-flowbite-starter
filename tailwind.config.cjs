000718d4984598a8e35bf56ac2d34fa03ec8c533
Amit K <androidak0r@gmail.com>
2024-01-16 21:33:08 +0530

    feat: :sparkles: Added custom components, layouts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
@@ -9,11 +9,11 @@ module.exports = {
   ],
   theme: {
     colors: {
-      primary: "#17202A",
-      secondary: "#ecc94b",
+      primary: "#F16E0F",
+      secondary: "#247084",
       transparent: "transparent",
       current: "currentColor",
-      slate: colors.slate,
+      slate: colors.zinc,
       stone: colors.stone,
       blue: colors.blue,
       green: colors.green,
@@ -23,24 +23,39 @@ module.exports = {
       emerald: colors.emerald,
       indigo: colors.indigo,
       yellow: colors.yellow,
+      zinc: colors.zinc,
+      pumpkin: "#F16E0F",
+      tangerine: "#EF9A6B",
+      cerulean: "#247084",
+      bgLight: "#FBFBFB",
+      bgDark: "#151419",
+      txDark: "#1B1B1E",
+      txLight: "#D4D4D6"
     },
     extend: {
       translate: ["dark"],
       animation: {
         'skew-scroll': 'skew-scroll 20s linear infinite',
       },
+      fontFamily: {
+				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+			}
     },
-      keyframes: {
-        'skew-scroll': {
-          '0%': {
-            transform: 'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)',
-          },
-          '100%': {
-            transform:
-              'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)',
-          },
+    keyframes: {
+      'skew-scroll': {
+        '0%': {
+          transform: 'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(0)',
+        },
+        '100%': {
+          transform:
+            'rotatex(20deg) rotateZ(-20deg) skewX(20deg) translateZ(0) translateY(-100%)',
         },
       },
+    },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("flowbite/plugin")
+  ],
+  darkMode: "class"
 };
